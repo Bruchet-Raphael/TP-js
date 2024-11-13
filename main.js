092e9f4b8ebeeb66098855887921e5717256a510
titi <tata@gmail.com>
2024-11-13 15:31:21 +0100

    Exo-2

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 alert("Hello World");
+
+//chaine de caractère
+var MavariableString = "toto";
+//nombre
+var MavariableNumerique = 100;
+//des tableaux
+var MavariableTableau = ['Apple', 'Bananna'];
+//des objets (ici la fenetre du navigateur)
+var MavariableObjet = window;
+
+alert(MavariableString + MavariableNumerique + MavariableTableau[0] + MavariableObjet.location.pathname)
